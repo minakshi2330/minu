da0d0210e4a5a0203fbeb5264a60d722023eec4f
minakshi2330 <136163324+minakshi2330@users.noreply.github.com>
2024-01-25 17:40:52 +0530

    sre

---
+++ digits.rb
@@ -0,0 +1,22 @@
+# puts 12 + 12
+# puts "12" + "12"
+# puts "12 + 12"
+
+
+
+# puts 2* 5
+# puts "2" * 5
+# puts "2 * 5"
+
+
+# puts "they said, \"yes!\""
+# puts "up \\ down"
+# puts "backslash at the end of a string:\\"
+# puts "Dip!"
+# puts "\D\i\p\!"
+
+
+
+puts "Duck...\nDuck..."
+puts "\tGoose! (or grey duck for some of you)"
+
---
+++ letter.rb
@@ -0,0 +1,7 @@
+puts "hello world "
+puts ""
+puts "good-bye"
+
+puts "i like" + "apple pie"
+puts "i like" + "apple pie."
+puts "blink" * 4
