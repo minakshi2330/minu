e2ad460f57fb15ca45385de56243735f04edd751
minakshi2330 <136163324+minakshi2330@users.noreply.github.com>
2024-01-25 17:40:00 +0530

    hfgh

---
+++ var.rb
@@ -0,0 +1,44 @@
+# puts "...you can say that again..."
+# puts "...you can say that again..."
+
+# my_string = 
+# "...you can say that again..."
+# puts my_string
+# puts my_string
+
+
+# name = " minu minakshi kumari tiwari"
+# puts "my name is "+ name + "."
+# puts "wow!" + name + "\nis a really long name."
+
+
+# name = "minu minakshi kumari tiwari"
+#  puts "my name is #{name}."
+#  puts "wow #{name}\nis really long name"
+ 
+
+ # composer = "minakshi"
+ # puts "#{composer} is a very good girl and "
+ #  composer = "smart"
+ #  puts "#{composer} girl also she is an artist
+
+
+ # my_own_var = "just another" + " string"
+ # puts my_own_var
+
+ # my_own_var =5 * (1+2)
+ # puts my_own_var
+
+
+var1 =8
+var2 = var1
+
+puts var1
+puts var2
+
+puts ""
+
+var1 = "noice"
+
+puts var1
+puts var2
